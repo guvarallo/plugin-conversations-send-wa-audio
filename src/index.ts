c84fdd437f20a43f370c96a413b769f6c2f61451
Gus Varallo <gustavo@varallo.me>
2022-09-27 15:22:00 -0300

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import ConversationsSendWaAudioPlugin from './ConversationsSendWaAudioPlugin';
+
+FlexPlugin.loadPlugin(ConversationsSendWaAudioPlugin);
