f7a3976c62d80f4fc93a3dbbb84e47c8c364a2ae
Gus Varallo <gustavo@varallo.me>
2022-09-28 19:03:48 -0300

    chore: add styled components

---
+++ src/components/MediaMessage/MediaMessage.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const MediaMessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0.5rem 0;
+`
+
+export const MediaLink = styled.a`
+  margin: 0.5rem 0;
+`
--- src/components/MediaMessage/MediaMessage.tsx
+++ src/components/MediaMessage/MediaMessage.tsx
@@ -1,110 +1,22 @@
 import React from 'react'
 
+import { MediaLink, MediaMessageContainer } from './MediaMessage.styles'
+
 interface MediaMessageProps {
   mediaUrl: string
   mediaType: string
 }
 
-const source = 'audio/ogg'
-
 export const MediaMessage = ({
   mediaUrl,
   mediaType
 }: MediaMessageProps): JSX.Element => (
-  <>
-    {mediaType === source && (
-      <>
-        <audio controls>
-          <source src={mediaUrl} type={mediaType} />
-        </audio>
-        <a href={mediaUrl} target='_blank' rel='noopener noreferrer'>
-          Full Size Player
-        </a>
-      </>
-    )}
-  </>
+  <MediaMessageContainer>
+    <audio controls>
+      <source src={mediaUrl} type={mediaType} />
+    </audio>
+    <MediaLink href={mediaUrl} target='_blank' rel='noopener noreferrer'>
+      Full Size Player
+    </MediaLink>
+  </MediaMessageContainer>
 )
-
-// class MediaMessageComponent extends Component {
-//   renderImage = () => {
-//     const { mediaUrl } = this.props;
-
-//     return (
-//       <ImageWrapper>
-//         <img
-//           src={mediaUrl}
-//           alt='MMS'
-//           width='150px'
-//           onClick={() =>
-//             Flex.Actions.invokeAction('smsModalControl', {
-//               url: mediaUrl
-//             })
-//           }
-//         />
-//       </ImageWrapper>
-//     );
-//   };
-
-//   renderAudioPlayer = () => {
-//     const { mediaUrl, mediaType } = this.props;
-
-//     return (
-//       <AudioPlayerWrapper>
-//         <audio controls>
-//           <source src={mediaUrl} type={mediaType} />
-//         </audio>
-//         <a href={mediaUrl} target='_blank' rel='noopener noreferrer'>
-//           Full Size Player
-//         </a>
-//       </AudioPlayerWrapper>
-//     );
-//   };
-
-//   renderPdfViewer = () => {
-//     const { mediaUrl } = this.props;
-
-//     return (
-//       <PdfViewerWrapper>
-//         <iframe title='PDF Preview' src={mediaUrl} width='100%' />
-//         <a href={mediaUrl} target='_blank' rel='noopener noreferrer'>
-//           Full Size Document
-//         </a>
-//       </PdfViewerWrapper>
-//     );
-//   };
-
-//   renderVideoPlayer = () => {
-//     const { mediaUrl, mediaType } = this.props;
-
-//     return (
-//       <VideoPlayerWrapper>
-//         <video width='100%' controls>
-//           <source src={mediaUrl} type={mediaType} />
-//         </video>
-//         <a href={mediaUrl} target='_blank' rel='noopener noreferrer'>
-//           Full Size Player
-//         </a>
-//       </VideoPlayerWrapper>
-//     );
-//   };
-
-//   render() {
-//     const { mediaType } = this.props;
-
-//     switch (mediaType) {
-//       case 'image/jpeg':
-//       case 'image/png':
-//         return this.renderImage();
-//       case 'audio/mpeg':
-//       case 'audio/ogg':
-//       case 'audio/amr':
-//         return this.renderAudioPlayer();
-//       case 'application/pdf':
-//         return this.renderPdfViewer();
-//       case 'video/mp4':
-//         return this.renderVideoPlayer();
-//       default:
-//         return <div />;
-//     }
-//   }
-// }
